d645a959584b97870b399d22fe9d0cbf18db5e74
Luis Ezcurdia <ing.ezcurdia@gmail.com>
2022-07-29 17:49:13 -0500

    Accepts nested attributes for site

--- app/models/competitor.rb
+++ app/models/competitor.rb
@@ -5,6 +5,7 @@ class Competitor < ApplicationRecord
   belongs_to :site, optional: true
   has_many_attached :logo
   has_many_attached :picture
+  accepts_nested_attributes_for :site, allow_destroy: false, reject_if: ->(attrs) { attrs["url"].blank? }
 
   delegate :url, :data, to: :site, prefix: true, allow_nil: true
 end
--- test/models/competitor_test.rb
+++ test/models/competitor_test.rb
@@ -3,7 +3,20 @@
 require "test_helper"
 
 class CompetitorTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def competitor
+    @competitor ||= competitors(:one)
+  end
+
+  test "must be valid" do
+    assert_predicate competitor, :valid?
+  end
+
+  test "must allow nested attributes for site" do
+    competitor = Competitor.new(name: "El Bajio", business: businesses(:one),
+                                site_attributes: { url: "http://restauranteelbajio.com.mx/" })
+    assert_predicate competitor, :valid?
+    assert_difference "Site.count", 1 do
+      competitor.save
+    end
+  end
 end
