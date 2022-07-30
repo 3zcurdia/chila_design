1571ab8ceb4c4a0a650ab813da37b7e9efedbc44
Luis Ezcurdia <ing.ezcurdia@gmail.com>
2022-07-30 12:22:22 -0500

    Set dimensions example

--- app/models/site.rb
+++ app/models/site.rb
@@ -2,12 +2,12 @@
 
 class Site < ApplicationRecord
   has_many :competitors, dependent: :nullify
-  
+
   has_one_attached :screenshot do |attachable|
     attachable.variant :thumb, resize_to_limit: [150, 150]
     attachable.variant :brand, resize_to_fill: [256, 256], gravity: "NorthWest"
   end
-  
+
   validates :url, presence: true, uniqueness: true
 
   after_create :scrape_site, if: -> { url_changed? || data.blank? }
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# frozen_string_literal: true
 
-require 'csv'
+require "csv"
+
+["food & beberage"].each do |name|
+  Industry.find_or_create_by(name: name)
+end
 
 [
-  {thesis: "Professional", antithesis: "Casual"},
-  {thesis: "Fun", antithesis: "Informative"},
-  {thesis: "Enthusiastic", antithesis: "Serious"},
-  {thesis: "Upbeat", antithesis: "Calm"},
-  {thesis: "Nostalgic", antithesis: "Modern"},
+  { thesis: "Professional", antithesis: "Casual" },
+  { thesis: "Fun", antithesis: "Informative" },
+  { thesis: "Enthusiastic", antithesis: "Serious" },
+  { thesis: "Upbeat", antithesis: "Calm" },
+  { thesis: "Nostalgic", antithesis: "Modern" }
 ].each do |quality|
   Quality.find_or_create_by(quality)
 end
@@ -25,7 +23,38 @@ CSV.open(Rails.root.join("config/pantones.csv"), headers: true).each do |row|
 end
 
 [
-  "food & beberage", 
-].each do |name|
-  Industry.find_or_create_by(name: name)
+  {
+    name: "Raleway, sans-serif",
+    dimensions: [
+      { quality_thesis: "Professional", value: 0.1e2 },
+      { quality_thesis: "Fun", value: 0.1e1 },
+      { quality_thesis: "Enthusiastic", value: 0.2e1 },
+      { quality_thesis: "Upbeat", value: 0.4e1 },
+      { quality_thesis: "Nostalgic", value: 0.9e1 }
+    ]
+  }
+].each do |fnt|
+  font = Font.find_or_create_by(name: fnt[:name])
+  fnt[:dimensions].each do |dim|
+    font.dimensions.joins(:quality).find_by(quality: { thesis: dim[:quality_thesis] }).update(value: dim[:value])
+  end
+end
+
+[
+  {
+    name: "Verde Tono",
+    value: "#007041",
+    dimensions: [
+      { quality_thesis: "Professional", value: 0.5e1 },
+      { quality_thesis: "Fun", value: 0.8e1 },
+      { quality_thesis: "Enthusiastic", value: 0.7e1 },
+      { quality_thesis: "Upbeat", value: 0.8e1 },
+      { quality_thesis: "Nostalgic", value: 0.6e1 }
+    ]
+  }
+].each do |clr|
+  color = Color.find_or_create_by(name: clr[:name], value: clr[:value])
+  clr[:dimensions].each do |dim|
+    color.dimensions.joins(:quality).find_by(quality: { thesis: dim[:quality_thesis] }).update(value: dim[:value])
+  end
 end
