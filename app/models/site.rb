7eb5b856dafabe0e0c202ef3cf348408033359fc
Luis Ezcurdia <ing.ezcurdia@gmail.com>
2022-07-30 10:52:54 -0500

    Trigger background scrapper

--- app/models/site.rb
+++ app/models/site.rb
@@ -2,10 +2,19 @@
 
 class Site < ApplicationRecord
   has_many :competitors, dependent: :nullify
-  validates :url, presence: true, uniqueness: true
-
+  
   has_one_attached :screenshot do |attachable|
     attachable.variant :thumb, resize_to_limit: [150, 150]
     attachable.variant :brand, resize_to_fill: [256, 256], gravity: "NorthWest"
+  end
+  
+  validates :url, presence: true, uniqueness: true
+
+  after_create :scrape_site, if: -> { url_changed? || data.blank? }
+
+  private
+
+  def scrape_site
+    ScrapeSiteJob.perform_later(self)
   end
 end
